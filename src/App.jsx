e91035fd23eae96698c0e2cc5abcaced027dde57
Muhammad Huzaifa Ghori <huzaifaghori000@gmail.com>
2023-09-15 16:12:19 +0500

    committing from local

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Huzaifa Ghori",
+  title: "Web Developer & Data Engineer",
+  email: "Huzaifa.Ghori@studentambassadors.com",
+  gitHub: "huzaifa1910",
+  instagram: "huzaifa.ghori",
+  linkedIn: "Huzaifaghori",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
